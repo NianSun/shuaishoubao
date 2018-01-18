22c193e543aabcf8148562df0d881cb3a773f59f
Administrator <Administrator@YLMF-1801101159>
2018-01-18 09:06:10 +0800

    额

---
+++ shuaishoubao/js/oYrz.js
@@ -0,0 +1,25 @@
+$(function(){
+		
+	adaptation();
+	
+	//点击上传图片；
+	$(".file_css").change(function(){
+		var oVal=$(this).val()
+//		alert($(this).parent().find('div').eq(0).html());
+		$(this).parent().find('div').eq(0).html(oVal);
+	})
+	
+});
+window.onload=function(){
+	var height_img=$(".shangchuan_img img").height()/2+$(".file_css").height()/2;
+	$(".file_css").css({'top':-height_img})
+}
+
+function adaptation(){
+	// // 获取设备大小自适应
+	var v_Height=$(window).height();
+	var v_Width=$(window).width();
+	
+	$("body").css({'font-size':v_Width*0.03});
+	
+}
